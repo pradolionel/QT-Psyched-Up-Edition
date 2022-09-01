9ddac77ab6035fb33634c15288365053d3d51f76
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-09-01 17:44:24 +0300

    Thingy

--- source/Main.hx
+++ source/Main.hx
@@ -7,6 +7,10 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+
 	public static var fpsVar:FPS;
 
 	public function new()
@@ -19,7 +23,19 @@ class Main extends Sprite
 
 		ClientPrefs.loadDefaultKeys();
 
-		addChild(new FlxGame(0, 0, TitleState, 1, 60, 60, true, false));
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (zoom == -1)
+		{
+			var ratioX:Float = stageWidth / gameWidth;
+			var ratioY:Float = stageHeight / gameHeight;
+			zoom = Math.min(ratioX, ratioY);
+			gameWidth = Math.ceil(stageWidth / zoom);
+			gameHeight = Math.ceil(stageHeight / zoom);
+		}
+
+		addChild(new FlxGame(gameWidth, gameHeight, TitleState, zoom, 60, 60, true, false));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
